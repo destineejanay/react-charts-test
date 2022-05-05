083c3256c6cca15924142c9a627eb0f9952930d7
Destinee Muse <destineemuse20@yahoo.com>
2022-05-04 22:50:48 -0500

    second commit, added charts

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,29 @@
 import * as React from "react";
 import DeviceChart from "./DeviceChart";
 import Layout from "./Layout";
+import { name } from "../utils/generators";
+import RadialChart from "./RadialGradientChart";
+import AreaChart from "./AreaChart";
 
 export default function Home() {
   return (
     <Layout>
-      <DeviceChart />
+      <h2>{name("Destinee", "Muse")}</h2>
+      <DeviceChart
+        options={{
+          colors: ["#008080", "#612b4f", "#ffb4cc"],
+          labels: ["Desktop", "Mobile", "Tablet"],
+        }}
+      />
+
+      <DeviceChart
+        options={{
+          colors: ["#ff6379", "#990033", "#752a20"],
+          labels: ["Smartphones", "Laptops", "iPads"],
+        }}
+      />
+      <AreaChart />
+      <RadialChart />
     </Layout>
   );
 }
